ee6e3a3c125896d67364c240d76daab658088c81
chyidl <chyidl.com@gmail.com>
2021-08-16 17:31:31 +0800

    add user services

---
+++ services/user/go.mod
@@ -0,0 +1,8 @@
+module github.com/chyidl/begin-go-micro/services/user
+
+go 1.16
+
+require (
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/micro/micro/v3 v3.3.0
+)
---
+++ services/user/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/chyidl/begin-go-micro/services/user/handler"
+	pb "github.com/chyidl/begin-go-micro/services/user/proto"
+
+	"github.com/micro/micro/v3/service"
+	"github.com/micro/micro/v3/service/logger"
+)
+
+func main() {
+	// Create service
+	srv := service.New(
+		service.Name("user"),
+		service.Version("latest"),
+	)
+
+	// Register handler
+	pb.RegisterUserHandler(srv.Server(), new(handler.User))
+
+	// Run service
+	if err := srv.Run(); err != nil {
+		logger.Fatal(err)
+	}
+}
