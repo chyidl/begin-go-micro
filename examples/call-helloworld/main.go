b8a05887ef417e0ba770c2692d1f64347dd6df2f
chyidl <chyidl@gmail.com>
2021-08-16 16:58:13 +0800

    update

--- examples/call-helloworld/main.go
+++ examples/call-helloworld/main.go
@@ -26,6 +26,7 @@ func main() {
 
 	// print the response
 	fmt.Println("Response: ", rsp.Msg)
+	fmt.Println("It's me")
 
 	// let's delay the process for existing for reason you'll see below
 	time.Sleep(time.Second * 5)
